c187ed9daf6cf104d7487f42c5a94b003a774c3a
Richard Gill <richard@rgill.co.uk>
2019-12-30 14:14:14 +0000

    updated to node 13

--- test/index.cjs
+++ test/index.cjs
@@ -2,7 +2,8 @@
 
 const { sync: globSync } = require('glob')
 ;(async () => {
-  const matches = globSync('test/**/*.mjs')
+  const matches = globSync('test/**/*.js')
+  console.log(matches)
   for (const match of matches) {
     await import('../' + match)
   }
